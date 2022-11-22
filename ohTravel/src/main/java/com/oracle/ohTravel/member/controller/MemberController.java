62d4dec1668c56ebd4162c98e159fbd966dd7548
sbnlo <sbnlo@YERIM>
2022-11-22 16:14:59 +0900

    마이페이지 예약 내역

--- ohTravel/src/main/java/com/oracle/ohTravel/member/controller/MemberController.java
+++ ohTravel/src/main/java/com/oracle/ohTravel/member/controller/MemberController.java
@@ -34,8 +34,62 @@ public class MemberController {
 		return "member/findPassword";
 	}
 	
+	// 마이페이지 메인
 	@GetMapping(value = "/myPageMain")
 	public String goMyPageMain() {
 		return "member/myPageMain";
+	}
+	// 패키지 예약 내역
+	@GetMapping(value = "/myPageReservPackage")
+	public String goMyPageReservPackage() {
+		return "member/myPageReservPackage";
+	}
+	
+	// 호텔 예약 내역
+	@GetMapping(value = "/myPageReservHotel")
+	public String goMyPageReservHotel() {
+		return "member/myPageReservHotel";
+	}
+	
+	// 항공 예약 내역
+	@GetMapping(value = "/myPageReservAir")
+	public String goMyPageReservAir() {
+		return "member/myPageReservAir";
+	}
+	
+	// 티켓 예약 내역
+	@GetMapping(value = "/myPageReservTicket")
+	public String goMyPageReservTicket() {
+		return "member/myPageReservTicket";
+	}
+	
+	// 찜
+	@GetMapping(value = "/myPageLike")
+	public String goMyPageLike() {
+		return "member/myPageLike";
+	}
+	
+	// 쿠폰함
+	@GetMapping(value = "/myPageCoupon")
+	public String goMyPageCoupon() {
+		return "member/myPageCoupon";
+	}
+	
+	// 자주 찾는 질문
+	@GetMapping(value = "/myPageFAQ")
+	public String goMyPageFAQ() {
+		return "member/myPageFAQ";
+	}
+	
+	// My 상품평
+	@GetMapping(value = "/myPageReview")
+	public String goMyPageReview() {
+		return "member/myPageReview";
+	}
+	
+	// 개인정보
+	@GetMapping(value = "/myPagePrivacy")
+	public String goMyPagePrivacy() {
+		return "member/myPagePrivacy";
 	}
 }
