7044e0c6ff49e7efd9bf16a85721e76e985e032f
bono039 <bono039@hanyang.ac.kr>
2022-12-13 18:14:50 +0900

    1213 예약 완....

--- ohTravel/src/main/java/com/oracle/ohTravel/ticket/controller/TicketController.java
+++ ohTravel/src/main/java/com/oracle/ohTravel/ticket/controller/TicketController.java
@@ -1,5 +1,9 @@
 package com.oracle.ohTravel.ticket.controller;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServlet;
@@ -7,11 +11,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.oracle.ohTravel.member.model.MemberDTO;
 import com.oracle.ohTravel.ticket.model.TicketDTO;
@@ -20,6 +26,7 @@ import com.oracle.ohTravel.ticket.service.Paging;
 import com.oracle.ohTravel.ticket.service.TicketService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
@@ -56,7 +63,7 @@ public class TicketController {
 		return "ticket/exhibitionMain";
 	}
 	
-	// 입장권 검색 결과
+	// 입장권 검색 결과 (안 할 수도...)
 	@GetMapping(value = "/exhSearchResult")
 	public String goExhSearchResult() {
 		return "ticket/exhSearchResult";
@@ -69,62 +76,35 @@ public class TicketController {
 		
 		TicketDTO ticketDTO = ts.getTicketDetail(ticket_id);
 		model.addAttribute("ticketDetail", ticketDTO);
-		
+		model.addAttribute("rv_real_id", ticketDTO.getTicket_id());
 		return "ticket/exhibitionDetail";
 	}
 
 	
-	/* 1트) 입장권 예약 페이지 */
+	/* 입장권 예약 페이지 (실제 예약 X. 전 페이지에서 필요한 정보만 가져오는) */
 	@PostMapping(value = "/exhPayment")
-	public String goExhibitionReservation(Model model, TicketDTO ticketDTO) {
+	public String goExhibitionReservation(TicketDTO ticketDTO, Model model) {
 		
+		model.addAttribute("ticket_id", ticketDTO.getTicket_id());
 		model.addAttribute("ticket_name", ticketDTO.getTicket_name());
 		model.addAttribute("totalPay", ticketDTO.getTotalPay());
 		model.addAttribute("adultCnt", ticketDTO.getAdultCnt());
 		model.addAttribute("childCnt",  ticketDTO.getChildCnt());
 		model.addAttribute("adDate", ticketDTO.getAdDate());
-		
-		
+
 		return "ticket/exhPayment";
 	}
 	
-	
-	// 2트) 입장권 예약 페이지
-	@GetMapping("/reservation")
-	public String ticketReservation(TicketReservation tcReserve, Model model, HttpServletRequest request, HttpSession session) {
-		log.info("== TicketController reservation() start ==");
-		log.info("tcReserve = " + tcReserve);
+	/* 실제 결제 */
+	@PostMapping(value = "/realReserve")
+	public String reserveExhibition(TicketReservation trDTO) {
+		System.out.println("== TicketController Start reserveExhibition ==");
+
+		ts.reserveExhibition(trDTO);
+
+		System.out.println(trDTO);
+		//@Value("${ticket_admission_date}")
 		
-		// 데이터 전달 확인 검사
-		if(tcReserve.getTicket_order_id() == null) {
-			String url = request.getRequestURI();
-			return "redirect:" + url;
-		}
-		
-		// 현재 로그인하고 있는 사용자 정보
-		MemberDTO memberDTO = (MemberDTO) session.getAttribute("res");
-		log.info("memberDTO = " + memberDTO);
-		
-		if(memberDTO == null) {
-			return "redirect:/member/loginForm";
-		}
-		
-		try {
-			// 입장권 상세 가져오기
-			TicketDTO ticket_detailDTO = ts.getTicketDetail(tcReserve.getTicket_id());
-			
-			// 총 가격 만들어주기
-			tcReserve.makeTotalPay(ticket_detailDTO.getTicket_adult_price(), ticket_detailDTO.getTicket_child_price());
-			
-			model.addAttribute("ticket_detailDTO", ticket_detailDTO);
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		model.addAttribute("memberDTO", memberDTO);
-		model.addAttribute("tcReserve", tcReserve);
-		log.info("== TicketController reservation() end ==");
-		return "ticket/reservation";
+		return "ticket/exhReserveSuccess";
 	}
 }
