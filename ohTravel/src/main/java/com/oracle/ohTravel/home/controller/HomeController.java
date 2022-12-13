0b8b5dff743c8581805efff7f5cf0cfadca861fe
startfrombt <82436530+startfrombt@users.noreply.github.com>
2022-12-13 17:04:20 +0900

    메인 index - 해외 패키지, 국내 패키지 3개 추가 (호텔 입장권 div 에 추가하시면 됩니다.)

--- ohTravel/src/main/java/com/oracle/ohTravel/home/controller/HomeController.java
+++ ohTravel/src/main/java/com/oracle/ohTravel/home/controller/HomeController.java
@@ -1,16 +1,55 @@
 package com.oracle.ohTravel.home.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.oracle.ohTravel.pkage.model.PkageDTO;
+import com.oracle.ohTravel.pkage.service.PkageService;
 
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+	@Autowired
+	PkageService pkageService;
 
 	@GetMapping(value = "/")
-	public String goHome() {
+	public String goHome(Model model) {
+		try {
+			// 해외 패키지 3개
+			// 해외 패키지 상품 판매순 / 평점순 3개 가져오기
+			Map<String, Integer> map = new HashMap<>();
+			map.put("pkage_gubun", 1);
+			map.put("row_count", 3);
+			List<PkageDTO> abroadPkageList = pkageService.selectPkgBySoldScoreOrder(map);
+			
+			// 국내 패키지 3개
+			Map<String, Integer> map2 = new HashMap<>();
+			map2.put("pkage_gubun", 0);
+			map2.put("row_count", 3);
+			List<PkageDTO> domesticPkageList = pkageService.selectPkgBySoldScoreOrder(map2);
+			
+			// 호텔 3개
+			
+			
+			// 입장권 3개
+			
+			
+			model.addAttribute("abroadPkageList", abroadPkageList);
+			model.addAttribute("domesticPkageList", domesticPkageList);
+			model.addAttribute("abroadGubun", 1);
+			model.addAttribute("domesticGubun", 0);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		return "main/home";
 	}
 	
